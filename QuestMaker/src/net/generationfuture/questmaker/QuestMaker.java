885cc1494d1013250568760fde3ff243494c105a
JuKu <JustinKuenzel@aol.com>
2012-01-21 13:44:09 +0100

    Dateien editiert.

--- QuestMaker/src/net/generationfuture/questmaker/QuestMaker.java
+++ QuestMaker/src/net/generationfuture/questmaker/QuestMaker.java
@@ -1,5 +1,7 @@
 package net.generationfuture.questmaker;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.newdawn.slick.*;
 import org.newdawn.slick.tiled.*;
 import org.newdawn.slick.Color;
@@ -26,6 +28,7 @@ public class QuestMaker extends BasicGame {
     @Override
     public void init(GameContainer gc) throws SlickException {
         start_image = new Image("Data/images/start_image.png");
+        gc.getInput().addMouseListener(new AppMouseListener(this));
     }
 
     @Override
@@ -34,17 +37,93 @@ public class QuestMaker extends BasicGame {
     }
 
     @Override
-    public void render(GameContainer gc, Graphics grphcs) throws SlickException {
+    public void render(GameContainer gc, Graphics g) throws SlickException {
         
         if (!start) {
             app.setDisplayMode(600, 400, false);
             this.start_image.draw(1, 1);
+        } else {
+            
+            g.fillRect(1, 1, 800, 600);
+            g.setColor(Color.blue);
+            g.drawString("GFGame-QuestMaker", 20, 20);
+            g.setColor(Color.white);
+            
         }
         
     }
     
-    public class MouseListener {
-        //
+    class AppMouseListener implements MouseListener {
+        
+        QuestMaker questmaker;
+        
+        public AppMouseListener (QuestMaker questmaker) {
+            this.questmaker = questmaker;
+        }
+
+        @Override
+        public void mouseWheelMoved(int i) {
+            //
+        }
+
+        @Override
+        public void mouseClicked(int i, int i1, int i2, int i3) {
+            
+            if (!questmaker.start) {
+                
+                try {
+                    
+                    questmaker.start = true;
+                    QuestMaker.app.setDisplayMode(800, 600, false);
+                    
+                } catch (SlickException ex) {
+                    Logger.getLogger(QuestMaker.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+        }
+
+        @Override
+        public void mousePressed(int i, int i1, int i2) {
+            //throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void mouseReleased(int i, int i1, int i2) {
+            //throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void mouseMoved(int i, int i1, int i2, int i3) {
+            //throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void mouseDragged(int i, int i1, int i2, int i3) {
+            //throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void setInput(Input input) {
+            //throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public boolean isAcceptingInput() {
+            //throw new UnsupportedOperationException("Not supported yet.");
+            return true;
+        }
+
+        @Override
+        public void inputEnded() {
+            //throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void inputStarted() {
+            //throw new UnsupportedOperationException("Not supported yet.");
+        }
+        
     }
     
 }
