0940c9da4ea45cad7819e80c120dda559e8b3a98
JuKu <JustinKuenzel@aol.com>
2012-01-21 13:25:40 +0100

    Dateien editiert.

--- QuestMaker/src/net/generationfuture/questmaker/QuestMaker.java
+++ QuestMaker/src/net/generationfuture/questmaker/QuestMaker.java
@@ -1,11 +1,38 @@
 package net.generationfuture.questmaker;
 
 import org.newdawn.slick.*;
+import org.newdawn.slick.tiled.*;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
 
-public class QuestMaker {
+public class QuestMaker extends BasicGame {
+    
+    private Image start_image = null;
+    
+    public QuestMaker () throws SlickException {
+        super("GFGame-QuestMaker");
+    }
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws SlickException {
+         AppGameContainer app = new AppGameContainer(new QuestMaker());
+         //Dies ist JuKus erster Kommentar in dieser Datei. ;-)
+         app.setDisplayMode(800, 600, false);
+         app.start();
+    }
+
+    @Override
+    public void init(GameContainer gc) throws SlickException {
+        //
+    }
+
+    @Override
+    public void update(GameContainer gc, int i) throws SlickException {
+        //
+    }
+
+    @Override
+    public void render(GameContainer gc, Graphics grphcs) throws SlickException {
+        //
     }
     
 }
