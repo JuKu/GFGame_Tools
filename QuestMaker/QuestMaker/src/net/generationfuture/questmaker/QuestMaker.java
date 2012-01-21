ba09c5a870d240505959885019adbd40a57bd751
JuKu <JustinKuenzel@aol.com>
2012-01-21 13:08:23 +0100

    Dateien hinzugefügt.

---
+++ QuestMaker/QuestMaker/src/net/generationfuture/questmaker/QuestMaker.java
@@ -0,0 +1,11 @@
+package net.generationfuture.questmaker;
+
+import org.newdawn.slick.*;
+
+public class QuestMaker {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
