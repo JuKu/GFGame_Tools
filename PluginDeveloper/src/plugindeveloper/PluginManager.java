55c57b73aed1d128519381bee16d5e78a21f5987
JuKu <JustinKuenzel@aol.com>
2012-03-05 20:08:21 +0100

    Klasse "PluginManager" bearbeitet.

--- PluginDeveloper/src/plugindeveloper/PluginManager.java
+++ PluginDeveloper/src/plugindeveloper/PluginManager.java
@@ -4,5 +4,9 @@ public interface PluginManager {
     
     void showVisualMessage(String message);
     void showErrorMessage(String message);
+    String getPluginDataPath(String pluginname);
+    Boolean createPluginDataPath(String pluginname);
+    void writeLog(String message);
+    void showMessage(String message);
     
 }
